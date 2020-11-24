64e09a74677697a13b5735102ececbf120c33c77
reni11111 <renaldo@tegeria.com>
2020-11-24 01:58:40 +0100

    demo init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let allStudents= [{
+  name: "Renaldo",
+  age: 22
+}]
+
+app.post('/students', (req, res, next) => {
+  console.log(req.body)
+  const newStudent = req.body
+
+  allStudents.push(newStudent)
+  res.status(200).json(newStudent)
+})
+
+app.get('/students', (req, res, next) => {
+  res.status(200).json(allStudents)
+})
+
+
+const port = 5001
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
